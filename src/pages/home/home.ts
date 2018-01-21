408cb6c10127544e933e854e14a00c7daca6afe8
Jason Lee <jason.khn.lee@gmail.com>
2018-01-21 18:30:39 -0500

    Added some more charts for practice

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,19 +15,25 @@ export class HomePage {
         id: 'chart-1',
         data: {
           type: 'bar',
+          title: {
+            text: "Bar Chart"
+          },
           series: [
             {
               values: [1, 2, 3]
             }
           ]          
         },
-        height: 400,
+        height: '100%',
         width: '100%'
       },
       {
         id: 'chart-2',
         data: {
           type: 'scatter',
+          title: {
+            text: "Scatter Chart"
+          },
           series: [
             {
               values: [
@@ -39,7 +45,41 @@ export class HomePage {
             }
           ]
         },
-        height: 400,
+        height: '100%',
+        width: '100%'
+      },
+      {
+        id: 'chart-3',
+        data: {
+          type: 'pie',
+          title: {
+            text: "Pie Chart"
+          },
+          series: [
+            {values: [36]},
+            {values: [11]},
+            {values: [16]},
+            {values: [20]},
+            {values: [17]}
+          ]
+        },
+        height: '100%',
+        width: '100%'
+      },
+      {
+        id: 'chart-4',
+        data: {
+          type: 'line',
+          title: {
+            text: "Line Chart"
+          },
+          series: [
+            { values: [20, 40, 25, 50, 15, 45, 33, 34] },
+            { values: [5, 30, 21, 18, 59, 50, 28, 33] },
+            { values: [30, 5, 18, 21, 33, 41, 29, 15] }
+          ]
+        },
+        height: '100%',
         width: '100%'
       }
     ];
